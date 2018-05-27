f74172592d3f387885fe735d4db8799923853573
JackChinaY <2802180907@qq.com>
2018-05-27 16:47:35 +0800

    20180527，Daily Update

--- src/main/java/TEST/Alibaba/Main.java
+++ src/main/java/TEST/Alibaba/Main.java
@@ -1,8 +1,13 @@
 package TEST.Alibaba;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+    public static void main(String[] args) {
+        String[] temp = "1,2,3,4,5,6".split(",");
+        System.out.println(Arrays.toString(temp));
+    }
 }
